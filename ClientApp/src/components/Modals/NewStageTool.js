a324ec4fb5bab0ab8e467a58c23df83cfdf4cf32
BhavanaHexaware <bhavanan@hexaware.com>
2023-01-05 07:48:57 +0000

    New stage tool

--- ClientApp/src/components/Modals/NewStageTool.js
+++ ClientApp/src/components/Modals/NewStageTool.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect, useState }  from 'react';
 import PropTypes from 'prop-types';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
@@ -50,10 +50,74 @@ BootstrapDialogTitle.propTypes = {
   onClose: PropTypes.func.isRequired,
 };
 
-export default function NewStageTool({open, setOpen}) {
+export default function NewStageTool({open, setOpen }) {
 
 
   const handleClose = () => {
+    setOpen(false);
+  };
+
+  const [pipelineStages, setPipelineStages] = useState([]);
+
+  const fetchData = () => {
+   return fetch("https://52.146.8.157:7244/api/pipelinestages/")
+     .then((response) => response.json())
+     .then((data) => setPipelineStages(data));
+  }
+
+  const [supportedTools, setSupportedTools] = useState([]);
+    
+  const fetchTool = () => {
+   return fetch("https://52.146.8.157:7244/api/supporttools/")
+     .then((response) => response.json())
+     .then((tool) => setSupportedTools(tool));
+  }
+  useEffect(() => {
+    fetchData()
+    fetchTool()
+  }, [])
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [accessurl, setAccessurl] = useState("");
+  const [credtype, setCredtype] = useState("");
+  const [creduser, setCreduser] = useState("");
+  const [credsecret, setCredsecret] = useState("");
+  const [toolid, setToolid] = useState("");
+  const [stageid, setStageid] = useState("");
+  let handleSave = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("https://52.146.8.157:7244/api/stagetool/", {
+        method: "POST",
+        body: JSON.stringify({
+          name: name,
+          description: description, 
+          access_url: accessurl,
+          cred_type: credtype,
+          cred_user: creduser,
+          cred_secret: credsecret,
+          tool_id: 2,
+          stage_id:2
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setName("");
+        setDescription("");
+        setAccessurl("");
+        setCredtype("");
+        setCreduser("");
+        setCredsecret("");
+        setToolid("");
+        setStageid("");
+      } 
+    } catch (err) {
+      console.log(err);
+    }
     setOpen(false);
   };
 
@@ -78,7 +142,7 @@ export default function NewStageTool({open, setOpen}) {
         >
           <label>
             <span className='col-md-3'>Stage:</span>
-            <Tdropdown className='col-md-6'/>
+            <Tdropdown className='col-md-6' options={pipelineStages?.map((data) => data.name) || []} />
           </label>
         </Box>
         <Box
@@ -91,7 +155,7 @@ export default function NewStageTool({open, setOpen}) {
         >
           <label>
             <span className='col-md-3'>Tool:</span>
-            <Tdropdown className='col-md-6'/>
+            <Tdropdown className='col-md-6' options={supportedTools?.map((tool) => tool.name) || []}/>
           </label>
         </Box>
         <Box
@@ -104,7 +168,7 @@ export default function NewStageTool({open, setOpen}) {
         >
           <label>
             <span className='col-md-3'>Name:</span>
-            <TextField className='col-md-6' id="outlined-basic" label="Name" variant="outlined" />
+            <TextField className='col-md-6' id="outlined-basic" label="Name" onChange={(e) => setName(e.target.value)} variant="outlined" />
           </label>
         </Box>
         <Box
@@ -117,7 +181,7 @@ export default function NewStageTool({open, setOpen}) {
         >
           <label>
             <span className='col-md-3'>Description:</span>
-            <TextField className='col-md-6' id="outlined-basic" label="Description" variant="outlined" />
+            <TextField className='col-md-6' id="outlined-basic" label="Description" onChange={(e) => setDescription(e.target.value)} variant="outlined" />
           </label>
         </Box>
         <Box
@@ -130,7 +194,7 @@ export default function NewStageTool({open, setOpen}) {
         >
           <label>
             <span className='col-md-3'>Credential Type:</span>
-            <Tdropdown/>
+            <Tdropdown onChange={(e) => setCredtype(e.target.value)}/>
           </label>
         </Box>
         <Box
@@ -143,7 +207,7 @@ export default function NewStageTool({open, setOpen}) {
         >
           <label>
             <span className='col-md-3'>Access URL:</span>
-            <TextField className='col-md-6' id="outlined-basic" label="URL" variant="outlined" />
+            <TextField className='col-md-6' id="outlined-basic" label="URL" onChange={(e) => setAccessurl(e.target.value)} variant="outlined" />
           </label>
         </Box>
         <Box
@@ -156,7 +220,7 @@ export default function NewStageTool({open, setOpen}) {
         >
           <label>
             <span className='col-md-3'>User:</span>
-            <TextField className='col-md-6' id="outlined-basic" label="Id" variant="outlined" />
+            <TextField className='col-md-6' id="outlined-basic" label="Id" onChange={(e) => setCreduser(e.target.value)}  variant="outlined" />
           </label>
         </Box>
         <Box
@@ -169,12 +233,12 @@ export default function NewStageTool({open, setOpen}) {
         >
           <label>
             <span className='col-md-3'>Secret:</span>
-            <TextField className='col-md-6' id="outlined-basic" label="Secret" variant="outlined" />
+            <TextField className='col-md-6' id="outlined-basic" label="Secret" onChange={(e) => setCredsecret(e.target.value)}  variant="outlined" />
           </label>
         </Box>
         </DialogContent>
         <DialogActions>
-          <Button variant="contained" color="primary" autoFocus onClick={handleClose}>
+          <Button variant="contained" color="primary" autoFocus onClick={handleSave}>
             Save
           </Button>
         </DialogActions>
